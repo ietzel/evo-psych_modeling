4c1f3e8c4a4a1fb6bd2434d58f5e2dd5affea37c
ietzel <67884277+ietzel@users.noreply.github.com>
2024-02-01 09:41:41 -0800

    Update and rename basics.txt to main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+public class Main {
+  
+  vars = ["structural auto-evolution", "structural copying of information", "hypercycle-mediating interface", "multi-stage"]
+  public static void main(String args[]) {
+    
+  }
+}
