0eb81533511307e154987b6d8a14c0f3b1a7927c
ietzel <67884277+ietzel@users.noreply.github.com>
2024-02-01 10:05:07 -0800

    Update and rename main.js to main.java

---
+++ main.java
@@ -0,0 +1,8 @@
+public class Main {
+  
+  String[] vars = ["structural auto-evolution", "structural copying of information", "hypercycle-mediating interface", "multi-stage"];
+  //non-constant (i.e. not too specific), duplication of values between variables (that is, not too vague), saving-deleting cycle (meaning carriability in memory), arrayable (in other words, must be plenty of space in program)
+  public static void main(String args[]) {
+    
+  }
+}
--- main.js
+++
@@ -1,7 +0,0 @@
-public class Main {
-  
-  vars = ["structural auto-evolution", "structural copying of information", "hypercycle-mediating interface", "multi-stage"]
-  public static void main(String args[]) {
-    
-  }
-}
