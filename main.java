1ff2bbdc40fd701e27c4200278822fcf9a491325
ietzel <67884277+ietzel@users.noreply.github.com>
2024-02-01 17:36:12 -0800

    Update main.java

--- main.java
+++ main.java
@@ -2,6 +2,7 @@ public class Main {
   
   String[] vars = ["structural auto-evolution", "structural copying of information", "hypercycle-mediating interface", "multi-stage"];
   //non-constant (i.e. not too specific), duplication of values between variables (that is, not too vague), saving-deleting cycle (meaning carriability in memory), arrayable (in other words, must be plenty of space in program)
+  //attention (thoughts), words (actions), habits (character), destiny (environment)
   public static void main(String args[]) {
     
   }
