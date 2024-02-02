43d391163ff20cb875e2353a1d7b35c84a904877
ietzel <67884277+ietzel@users.noreply.github.com>
2024-02-02 11:15:45 -0800

    Update main.java

--- main.java
+++ main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Modeler {
     public static void main(String args[]) {
         String[] vars = {"structural auto-evolution", "structural copying of information", "hypercycle-mediating interface", "multi-stage"};
-        //non-constant (i.e. not too specific), duplication of values between variables (that is, not too vague), saving-deleting cycle (meaning carriability in memory), arrayable (in other words, must be plenty of space in program)
+        //non-constant (i.e. not too specific), duplication of values between variables (that is, not too vague), saving-deleting cycle (meaning secure carriability in memory), arrayable (in other words, must be plenty of space in program)
         //attention (thoughts), words (actions), habits (character), destiny (environment)
         Scanner s1 = new Scanner(System.in);
         System.out.println("name: ");
