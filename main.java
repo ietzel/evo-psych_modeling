4fa859eca6f6f24d0f80a6013eecc23e677239a4
ietzel <67884277+ietzel@users.noreply.github.com>
2024-02-07 12:45:50 -0800

    Update main.java

--- main.java
+++ main.java
@@ -1,15 +1,28 @@
-import java.util.Scanner;
 import processing.core.PApplet;
 
 public class Modeler extends PApplet {
     String[] vars = {"structural auto-evolution", "structural copying of information", "hypercycle-mediating interface", "multi-stage"};
     String[] organization = {"production", "R&D", "administration/management", "purchasing", "revenue"};
+    String[][] business_functions = {
+	    {"operations", "environmental sustainability", "supply chain", "product"},
+	    {"technology", "information security", "data analytics", "learning"},
+	    {"planning", "organizing & staffing", "controlling", "leading"},
+	    {"finance/accounting", "responsibility strategy", "HR & A", "UX & customer support"},
+	    {"sales/marketing", "communications", "compliance/legal", "party"},
+    };
     //non-constant (i.e. not too specific), duplication of values between variables (that is, not too vague), saving-deleting cycle (meaning secure carriability in memory), arrayable (in other words, must be plenty of space in program)
     //attention (thoughts), words (actions), habits (character), destiny (environment)
     public void settings() {
-	size(500, 500);
+	size(500, 400);
     }
     public void draw() {
+	fill(200);
+	for(int i = 0; i < organization.length; i++) {
+		text(organization[i], i*50, 0);
+		for(int j = 0; i < business_functions[i].length; j++) {
+			text(business_functions[i][j], i*50, j*40);
+		}
+	}
 	ellipse(mouseX, mouseY, 50, 50);
     }
     public void mousePressed() {
@@ -18,10 +31,6 @@ public class Modeler extends PApplet {
     public static void main(String[] args) {
 	String[] processingArgs = {"Modeler"};
 	Modeler Modeler = new Modeler();
-	Scanner s1 = new Scanner(System.in);
-	System.out.println("name: ");
-	String name = s1.nextLine();
-	System.out.println("");
 	PApplet.runSketch(processingArgs, Modeler);
     }
 }
