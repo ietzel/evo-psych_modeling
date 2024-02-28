631589bdfcd66efb8a5d9953a0851ef9acabfe8b
ietzel <67884277+ietzel@users.noreply.github.com>
2024-02-28 15:18:16 -0800

    Update main.java

--- main.java
+++ main.java
@@ -8,23 +8,25 @@ public class Modeler extends PApplet {
 	    {"technology", "information security", "data analytics", "learning"},
 	    {"planning", "organizing & staffing", "controlling", "leading"},
 	    {"finance/accounting", "responsibility strategy", "HR & A", "UX & customer support"},
-	    {"sales/marketing", "communications", "compliance/legal", "party"},
+	    {"sales/marketing", "communications", "compliance/legal", "party"}
     };
     //non-constant (i.e. not too specific), duplication of values between variables (that is, not too vague), saving-deleting cycle (meaning secure carriability in memory), arrayable (in other words, must be plenty of space in program)
     //attention (thoughts), words (actions), habits (character), destiny (environment)
-    public void settings() {
+    public void_settings() {
 	size(500, 400);
     }
+    const x_lent = 50;
+    const y_lent = 40;
     public void draw() {
 	fill(200);
 	for(int i = 0; i < organization.length; i++) {
-		text(organization[i], i*50, 0);
+		text(organization[i], i*x_lent, 0);
 		for(int j = 0; i < business_functions[i].length; j++) {
-			text(business_functions[i][j], i*50, j*40);
+			text(business_functions[i][j], i*x_lent, j*y_lent);
 		}
 	}
     }
-    boolean isMouseOver(int x, int y, int w, int h){
+    boolean isMouseOver(int x, int y, int w, int h) {
 	if(mouseX >= x && mouseX <= x + w && mouseY >= y && mouseY <= y + h) {
 		return true;
 	}
@@ -32,7 +34,18 @@ public class Modeler extends PApplet {
     }
 
     void mousePressed() {
-	if(isMouseOver(width/2,height/2,100,50) == true) {
+        for(int i = 0; i < organization.length; i++) {
+	    for(int j = -1; i < business_functions[i].length; j++) {
+	        if(isMouseOver(i*width/10,(j+1)*height/10,(i+1)*width/10,(j+2)*height/10) = true) {
+		    if(isMouseOver(width,height,0,height/10) == true) {
+		        println(business_functions[i][j]+" business function, maybe in image of "+organization[i]+"business function category.");
+		    } else {
+		        println(organization[i]+" business function category possibly in good shape.");
+		    }
+		}
+	    }
+        }
+	if(isMouseOver(width/2,height/2,100,50) = true) {
 		println("Mouse pressed button");
     		background = !background;
 	}
