16d986f8fed03718b3856ca97ed1ff0c7e494f49
ietzel <67884277+ietzel@users.noreply.github.com>
2024-05-06 18:39:36 -0700

    Update Modeler.rs

--- Modeler.rs
+++ Modeler.rs
@@ -15,7 +15,7 @@ let policies: [u8; 14] = {"anti-discrimination", "workplace health/safety/securi
 let conditions: [u8; 5] = {"compensation", "safety", "relationships", "skill discretion", "prospects"};
 
 fn calculate() {
-//
+	return 0;
 }
 
 slint::slint!{
